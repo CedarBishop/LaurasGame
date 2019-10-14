fcd8b6eb1bbeb45700fecb99d9cb85c1d6faad63
Cedar Bishop <47874454+CedarBishop@users.noreply.github.com>
2019-10-14 16:22:50 +1100

    init, created happy tile map and basic movement and teleporters

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    CircleCollider2D circleCollider;
+    Vector2 direction;
+    public float speed;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
+
+        rigidbody.gravityScale = 0;
+    }
+
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        direction = direction.normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = direction * speed * Time.fixedDeltaTime;
+    }
+
+
+}
---
+++ Assets/Teleporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class Teleporter : MonoBehaviour
+{
+    public Transform teleportLocation;
+    private CircleCollider2D circleCollider;
+
+    private void Start()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+        circleCollider.isTrigger = true;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>())
+        {
+            collision.GetComponent<PlayerController>().transform.position = teleportLocation.position;
+            gameObject.SetActive(false);
+        }
+    }
+}
